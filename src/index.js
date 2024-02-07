6dda203fdfa74baa5b4beaa18dbe1311d98fad72
adii1203 <aditya32ft@gmail.com>
2024-02-07 18:31:53 +0530

    Add dotenv configuration for environment variables

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import app from './app.js';
+import dotenv from 'dotenv';
+
+dotenv.config({
+  path: './.env',
+});
 
 app.on('error', (err) => {
   console.error('Server error:', err);
 });
 
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
+app.listen(process.env.PORT, () => {
+  console.log(`Server is listening on port ${process.env.PORT}`);
 });
